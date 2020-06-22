5a49e0f2378c9822a1816b209f3615ef0282c142
Jannosch Müller <janosch.mueller@betterplace.org>
2020-06-22 15:49:10 +0200

    Remove monkey patch after dep update

--- server/middlewares/item_crud.js
+++ server/middlewares/item_crud.js
@@ -1,14 +1,15 @@
 const { crud } = require('express-sequelize-crud')
-const { Op: { like, iLike } } = require('sequelize')
+const { searchFields } = require('express-sequelize-crud/lib/getList/searchList')
+const { Op } = require('sequelize')
 const { sequelize: { options: { dialect } } } = require('../models')
-const { searchFields } = require('../lib/search_list')
 const { Item } = require('../models')
 
-const searchMethod = dialect === 'sqlite' ? like : iLike
+const searchOp = dialect === 'sqlite' ? Op.like : Op.iLike
+const search = searchFields(Item, ['name', 'url'], searchOp)
 
 module.exports = () => (req, res, next) => {
   crud('/api/items', Item, {
-    search: searchFields(Item, ['name', 'url'], searchMethod),
+    search: (q, limit) => search(q, limit, { userId: req.user.id }),
     create: (body) => Item.create({ ...body, userId: req.user.id }),
   })(req, res, next)
 }
