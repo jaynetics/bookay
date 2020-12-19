0c946d246484ddbd7c210158cc8552d88acd57f8
Jannosch Müller <janosch.mueller@betterplace.org>
2020-12-19 22:22:42 +0100

    Try debugging flaky spec with more info

--- test/cypress/support/index.js
+++ test/cypress/support/index.js
@@ -1,10 +1,10 @@
 import './commands'
 
+// https://github.com/archfz/cypress-terminal-report
+require('cypress-terminal-report/src/installLogsCollector')()
+
 beforeEach(() => {
   cy.setLocalStorage('bookay_session_id', 'SoSafe')
   // clears db, sets up session with id 'SoSafe':
   cy.post({ url: 'http://localhost:3033/api/integration_test' })
-
-  // https://github.com/archfz/cypress-terminal-report
-  require('cypress-terminal-report/src/installLogsCollector')()
 })
